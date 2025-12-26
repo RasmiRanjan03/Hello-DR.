0e7dad07e0233eee040c8eb769fbaa92a5ae9c96
RasmiRanjan03 <sahoo.rasmi.ranjan9827@gmail.com>
2025-12-26 10:12:00 +0530

    just one left

--- Backend/controllers/admincontroller.js
+++ Backend/controllers/admincontroller.js
@@ -85,15 +85,5 @@ const alldoctors=async(req,res)=>{
     }
 }
 
-const changeavailability=async(req,res)=>{
-    try{
-        const {doctorId}=req.body;
-        const doctor=await doctormodel.findById(doctorId);
-        await doctormodel.findByIdAndUpdate(doctorId,{available:!doctor.available});
-        res.status(200).json({success:true,message:'Availability status updated'});     
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message:'Server Error'});
-    }
-}
-export{addDoctor,loginAdmin,logoutAdmin,alldoctors,changeavailability};
+
+export{addDoctor,loginAdmin,logoutAdmin,alldoctors};
